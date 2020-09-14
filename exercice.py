cdb51b64bad7f64e8889935f07ba680c531c1b3d
Maximus4321 <maxime.appert@polymtl.ca>
2020-09-14 23:41:58 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -27,8 +27,8 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 def to_degrees(angle_rads: float) -> tuple:
     Degré_complet = angle_rads *180/math.pi
-    degrees, minutes, Secondes = int(Degré_complet), int((Degré_complet % 1)*60), (Minutes%1)*60
-    return degrees, Minutes, Secondes
+    degrees, minutes, Secondes = int(Degré_complet), int((Degré_complet % 1)*60), (minutes%1)*60
+    return degrees, minutes, secondes
 
 
 def to_celsius(temperature: float) -> float:
